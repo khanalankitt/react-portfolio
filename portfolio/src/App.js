c78533a31cd84ff71c195fd6a420090f7a6d80cd
Ankit Khanal <akhanal749@gmail.com>
2023-09-10 21:25:56 +0545

    changed for hosting

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -10,7 +10,7 @@ import Footer from './footer';
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/khanalankitt">
         <Nav/>
       <Routes>
         <Route path='/' element={<Hero/>}/>
